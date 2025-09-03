d97d5f261dfeda84ad7cde8e0f6859e6c7c2f8b8
Bensaci11 <abdelkeddoussbensaci@gmail.com>
2025-09-03 23:11:22 +0100

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/books-of-madih/sw.js', { scope: '/books-of-madih/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
